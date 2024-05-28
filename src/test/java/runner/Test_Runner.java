8accfc02e494d25c6e5c757aab1f3535f73587cc
sathizk7 <sathizk7@192.168.1.111>
2024-05-28 13:24:00 -0400

    adding log4j2.properties, extent.properties and test runner class

--- src/test/java/runner/Test_Runner.java
+++ src/test/java/runner/Test_Runner.java
@@ -1,5 +1,22 @@
 package runner;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
-public class Test_Runner {
+	@CucumberOptions(
+			plugin = {"pretty", "html:target/cucumber.html", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}, //reporting purpose
+			
+			monochrome = true ,  // for console output clarity
+			
+			publish= true ,
+			
+			tags = "@signin", // tags from feature file
+			
+			features = {"src/test/resources/features"}, // location of feature files
+			
+			glue= {"dsalgo_stepdefinition", "dsalgo_hooks"} ) // location of step definition files
+	
 
+public class Test_Runner extends AbstractTestNGCucumberTests{
+	
+		
 }
