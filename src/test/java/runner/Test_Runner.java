627ebdef8c5dd022c8c85d7a888d02e6c6c36722
Sowmyahsn <sowmyahsn@gmail.com>
2024-06-11 20:11:22 -0400

    Update Test_Runner.java

--- src/test/java/runner/Test_Runner.java
+++ src/test/java/runner/Test_Runner.java
@@ -9,7 +9,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 	@CucumberOptions(
-			plugin = {"pretty", "html:target/cucumber.html","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}, //reporting purpose
+			plugin = {"pretty", "html:target/cucumber.html", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}, //reporting purpose
 			
 			monochrome = true ,  // for console output clarity
 		
